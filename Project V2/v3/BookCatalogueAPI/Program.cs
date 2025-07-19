556ef7bb7958af4a68bfb39dede3cced7a4aad92
Abhilash Laxmidhar <a13hi.a.s@gmail.com>
2025-07-19 23:08:10 +0530

    latest ver

---
+++ Project V2/v3/BookCatalogueAPI/Program.cs
@@ -0,0 +1,38 @@
+using BookCatalogueAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add Services
+builder.Services.AddControllers();
+builder.Services.AddDbContext<AppDbContext>(opt =>
+    opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// 🔁 Add CORS
+builder.Services.AddCors(opt => opt.AddPolicy("AllowReact",
+    b => b.WithOrigins("http://localhost:3000")
+          .AllowAnyHeader()
+          .AllowAnyMethod()));
+
+// Swagger
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// ✅ Apply CORS before anything else
+app.UseCors("AllowReact");
+
+// (Optional) Disable HTTPS redirection if not using https in dev
+// Comment out below if you don’t want redirect issues
+// app.UseHttpsRedirection();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+app.MapControllers();
+app.Run();
