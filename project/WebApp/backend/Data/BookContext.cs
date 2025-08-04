2007a9ce6f0b48354a73305e1768f1cd24830552
A13HILASH <127036032+A13HILASH@users.noreply.github.com>
2025-08-05 00:07:13 +0530

    Delete project directory

--- project/WebApp/backend/Data/BookContext.cs
+++
@@ -1,7 +0,0 @@
-using Microsoft.EntityFrameworkCore;
-
-public class BookContext : DbContext {
-    public BookContext(DbContextOptions<BookContext> options) : base(options) { }
-    public DbSet<Book> Books { get; set; }
-}
-
